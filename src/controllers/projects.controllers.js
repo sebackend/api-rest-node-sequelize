9306d11fa26c9bc3435063f3628a76f0ea8b3d45
Sebastian Meza <sbstnmz@gmail.com>
2022-06-06 23:34:29 -0400

    add: find Tasks for specific projectId in projects_controller

--- src/controllers/projects.controllers.js
+++ src/controllers/projects.controllers.js
@@ -1,4 +1,5 @@
 import { Project } from '../models/Project.js'
+import { Task } from '../models/Task.js'
 
 export const getProjects = async (req, res) => {
   try {
@@ -75,6 +76,22 @@ export const deleteProject = async (req, res) => {
     })
 
     res.sendStatus(204)
+  } catch (error) {
+    res.sendStatus(500).json({ message: error.message })
+  }
+}
+
+export const getProjectTasks = async (req, res) => {
+  try {
+    const { id } = req.params
+
+    const tasks = await Task.findAll({
+      where: {
+        projectId: id,
+      },
+    })
+
+    res.json(tasks)
   } catch (error) {
     res.sendStatus(500).json({ message: error.message })
   }
--- src/routes/projects.routes.js
+++ src/routes/projects.routes.js
@@ -5,6 +5,7 @@ import {
   createProject,
   updateProject,
   deleteProject,
+  getProjectTasks,
 } from '../controllers/projects.controllers.js'
 
 const router = Router()
@@ -14,5 +15,7 @@ router.get('/projects/:id', getProject)
 router.post('/projects', createProject)
 router.put('/projects/:id', updateProject)
 router.delete('/projects/:id', deleteProject)
+
+router.get('/projects/:id/tasks', getProjectTasks)
 
 export default router
